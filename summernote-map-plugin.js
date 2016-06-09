67682ef12f3678b06f2ea1573d6a726b908b5067
Maiyaporn Phanich <p.maiyaporn@gmail.com>
2016-06-09 15:44:00 -0500

    Updated script to lastest version

--- summernote-map-plugin.js
+++ summernote-map-plugin.js
@@ -31,6 +31,7 @@
         }
     });
 
+    // Define default option for map
     var mapOption = {
         apiKey: '', // google maps api browser key
         center: {
@@ -53,12 +54,14 @@
             var options = context.options;
             var lang = options.langInfo;
 
-            // Set default options for map
-           options.map = $.extend(mapOption, options.map);
+            // Extends default map option to options from user
+            options.map = $.extend(mapOption, options.map);
 
+            // Define Google Maps API URLs with user's apikey
             const GOOGLE_MAPS_API_URL = ("http://maps.google.com/maps/api/js?key=API_KEY&libraries=places").replace("API_KEY", options.map.apiKey);
             const EMBED_URL = ("https://www.google.com/maps/embed/v1/place?key=API_KEY&q=PLACE").replace("API_KEY", options.map.apiKey);
 
+            // Create a map button to be used in the toolbar
             context.memo('button.map', function() {
                 var button = ui.button({
                     contents: "<i class='fa fa-map-o'/>",
@@ -75,25 +78,10 @@
              * Create a dialog for adding map and append to container
              */
             this.initialize = function() {
-                var addMapDialog = {
-                    title: lang.mapDialog.title,
-                    body: '<div class="form-group">' +
-                                  '<label>' + 'Name or Address' + '</label>' +
-                                  '<input id="input-autocomplete" class="form-control" type="text" placeholder="Enter a place" />' +
-                               '</div>' +
-                               '<div id="map-in-dialog" style="height: 300px;"></div>',
-                    footer: '<button href="#" id="btn-add" class="btn btn-primary">' + 'Add' + '</button>' +
-                                '<button href="#" id="btn-cancel" class="btn btn-primary">' + 'Cancel' + '</button>',
-                    closeOnEscape: true
-                };
-
                 var $container = options.dialogsInBody ? $(document.body) : $editor;
-                self.$dialog = ui.dialog(addMapDialog).render().appendTo($container);
+                self.mapDialog.create($container);
 
-                $('.modal').css({
-                    "z-index": "20",
-                    "height": "100%"
-                });
+                $('.modal').css({ "z-index": "20", "height": "100%" });
             };
 
             this.destroy = function() {
@@ -106,76 +94,138 @@
                 // This will be called after modules are initialized.
                 'summernote.init': function (we, e) {
                     if( typeof google == 'object' && typeof google.maps == 'object' ) {
-                        self.initMapDialog();
+                        // self.initMapDialog();
+                        self.mapDialog.initMap();
                     } else {
-                        $.getScript( GOOGLE_MAPS_API_URL, self.initMapDialog);
+                        // $.getScript( GOOGLE_MAPS_API_URL, self.initMapDialog);
+                        $.getScript( GOOGLE_MAPS_API_URL, function() {self.mapDialog.initMap();});
                     }
                 }
             };
 
-            this.initMapDialog = function() {
-                console.log('initMapDialog');
-                var mapDiv = self.$dialog.find('#map-in-dialog')[0];
-                var map = new google.maps.Map(mapDiv, {
-                    center: options.map.center,
-                    zoom: options.map.zoom
-                });
-
-                var autocompleteInput = self.$dialog.find('#input-autocomplete')[0];
-                var autocomplete = new google.maps.places.Autocomplete(autocompleteInput);
-
-                autocomplete.bindTo('bounds', map);
-
-                var marker = new google.maps.Marker({
-                    map: map,
-                    anchorPoint: new google.maps.Point(0, -29)
-                });
-
-                google.maps.event.addListener(autocomplete, 'place_changed', function() {
-                    marker.setVisible(false);
-
-                    var place = autocomplete.getPlace();
-                    if (!place.geometry) {
-                        console.log("Autocomplete's returned place contains no geometry");
-                        return;
+            this.mapDialog = {
+                dialogOption: {
+                    title: lang.mapDialog.title,
+                    body: '<div class="form-group">' +
+                                  '<label>' + 'Name or Address' + '</label>' +
+                                  '<input id="input-autocomplete" class="form-control" type="text" placeholder="Enter a place" />' +
+                               '</div>' +
+                               '<div id="map-in-dialog" style="height: 300px;"></div>',
+                    footer: '<button href="#" id="btn-add" class="btn btn-primary">' + 'Add' + '</button>',
+                    closeOnEscape: true
+                },
+                create: function($container) {
+                    this.$dialog = ui.dialog(this.dialogOption).render().appendTo($container);
+                    this.$addBtn = this.$dialog.find('#btn-add');
+                    this.$mapInput = this.$dialog.find('#input-autocomplete')[0];
+                    this.$mapContainer = this.$dialog.find('#map-in-dialog')[0];
+                },
+                getAutoCompleteInput: function() {
+                    return this.$mapInput;
+                },
+                getMapContainer: function() {
+                    return this.$mapContainer
+                },
+                getAddButton: function() {
+                    return this.$addBtn[0];
+                },
+                enableAddButton: function() {
+                    if( this.$mapInput.value && this.$mapInput.value.length > 0 ) {
+                        this.$addBtn.attr("disabled", false);
                     }
+                },
+                disableAddButton: function() {
+                    this.$addBtn.attr("disabled", true);
+                },
+                show: function() {
+                    this.disableAddButton();
+                    this.$mapInput.value = "";
 
-                    // If the place has a geometry, then present it on a map.
-                    if (place.geometry.viewport) {
-                        map.fitBounds(place.geometry.viewport);
-                    } else {
-                        map.setCenter(place.geometry.location);
-                        map.setZoom(options.map.zoom);
-                    }
+                    return $.Deferred(function(deferred) {
+                        ui.onDialogShown(self.mapDialog.$dialog, function() {
+                            context.triggerEvent('dialog.shown');
+                            self.mapDialog.$mapInput.focus();
+                            google.maps.event.trigger(self.mapDialog.map, 'resize');
+                             $('.modal-backdrop').css("z-index", 10);
 
-                    marker.setIcon( /** @type {google.maps.Icon} */ ({
-                        url: place.icon,
-                        size: new google.maps.Size(71, 71),
-                        origin: new google.maps.Point(0, 0),
-                        anchor: new google.maps.Point(17, 34),
-                        scaledSize: new google.maps.Size(35, 35)
-                    }));
+                            self.mapDialog.$addBtn.click(function(event) {
+                                event.preventDefault();
+                                deferred.resolve({place: self.mapDialog.$mapInput.value});
+                            });
+                        });
 
-                    marker.setPosition(place.geometry.location);
-                    marker.setVisible(true);
-                });
+                        ui.onDialogHidden(self.mapDialog.$dialog, function() {
+                            self.mapDialog.marker.setVisible(false);
+                            self.mapDialog.$addBtn.off('click');
+                            if (deferred.state() === 'pending') {
+                                deferred.reject();
+                            }
+                        });
 
-                // google.maps.event.addListener(map, 'idle', function() {
-                //     google.maps.event.trigger(map, 'resize');
-                // });
+                        ui.showDialog(self.mapDialog.$dialog);
+                    });
+                },
+                showDialog: function() {
+                    ui.showDialog(this.$dialog);
+                },
+                hideDialog: function() {
+                    ui.hideDialog(this.$dialog);
+                },
+                initMap: function() {
+                    this.map = new google.maps.Map(this.$mapContainer, {
+                        center: options.map.center,
+                        zoom: options.map.zoom
+                    });
+
+                    this.autocomplete = new google.maps.places.Autocomplete(this.$mapInput);
+                    this.autocomplete.bindTo('bounds', this.map);
+
+                    this.marker = new google.maps.Marker({
+                        map: this.map,
+                        anchorPoint: new google.maps.Point(0, -29)
+                    });
+
+                    // Update map to show selected place and place marker
+                    google.maps.event.addListener(this.autocomplete, 'place_changed', function() {
+                        self.mapDialog.marker.setVisible(false);
+                        var place = self.mapDialog.autocomplete.getPlace();
+                        if (!place.geometry) {
+                            self.mapDialog.disableAddButton();
+                            console.log("Autocomplete's returned place contains no geometry");
+                            return;
+                        }
+
+                        // If the place has a geometry, then present it on a map.
+                        if (place.geometry.viewport) {
+                            self.mapDialog.map.fitBounds(place.geometry.viewport);
+                        } else {
+                            self.mapDialog.map.setCenter(place.geometry.location);
+                            self.mapDialog.map.setZoom(options.map.zoom);
+                        }
+
+                        self.mapDialog.marker.setIcon( /** @type {google.maps.Icon} */ ({
+                            url: place.icon,
+                            size: new google.maps.Size(71, 71),
+                            origin: new google.maps.Point(0, 0),
+                            anchor: new google.maps.Point(17, 34),
+                            scaledSize: new google.maps.Size(35, 35)
+                        }));
+
+                        self.mapDialog.marker.setPosition(place.geometry.location);
+                        self.mapDialog.marker.setVisible(true);
+                        self.mapDialog.enableAddButton();
+                    });
+                }
             };
 
             this.show = function() {
                 context.invoke('editor.saveRange');
 
-                this.showAddMapDialog()
-                    .then(function() {
+                self.mapDialog.show()
+                    .then(function(data) {
                         context.invoke('editor.restoreRange');
-                        var input = self.$dialog.find('#input-autocomplete')[0];
-
-                        self.insertEmbedMapToEditor(input.value);
-                        ui.hideDialog(self.$dialog);
-                        input.value = "";
+                        self.insertEmbedMapToEditor(data.place);
+                        self.mapDialog.hideDialog();
                     }).fail(function() {
                         context.invoke('editor.restoreRange');
                     });
@@ -187,9 +237,7 @@
                     'position': 'relative',
                     'padding-top': '25px',
                     'padding-bottom': '56.25%',
-                    'height': '0',
-                    'overflow': 'hidden',
-                    'text-align': 'center'
+                    'height': '0'
                 });
 
                 var $iframe = $('<iframe>', {
@@ -201,49 +249,12 @@
                     'position': 'absolute',
                     'top': '0',
                     'left': '0',
-                    'width': '100% !important',
-                    'height': '100% !important',
-                    'margin': '0 auto'
+                    'width': '60%',
+                    'height': '60%'
                 });
-
-                console.log($iframe)
 
                 $div.html($iframe)
-                // var iframe = document.createElement('iframe');
-                // iframe.width = "400";
-                // iframe.height = "300";
-                // iframe.src = EMBED_URL.replace("PLACE", placeName);
-
                 context.invoke('editor.insertNode',  $div[0]);
-            };
-
-            this.showAddMapDialog = function() {
-                return $.Deferred(function(deferred) {
-
-                    $addBtn = self.$dialog.find('#btn-add');
-
-                    ui.onDialogShown(self.$dialog, function() {
-                        // http://stackoverflow.com/questions/10957781/google-maps-autocomplete-result-in-bootstrap-modal-dialog
-                        $('.modal-backdrop').css("z-index", 10);
-                        context.triggerEvent('dialog.shown');
-
-                        $addBtn.click(function(event) {
-                            event.preventDefault();
-                            deferred.resolve();
-                        });
-
-                    });
-
-                    ui.onDialogHidden(self.$dialog, function() {
-                        $addBtn.off('click');
-
-                        if (deferred.state() === 'pending') {
-                            deferred.reject();
-                        }
-                    });
-
-                    ui.showDialog(self.$dialog);
-                });
             };
         }
     });
